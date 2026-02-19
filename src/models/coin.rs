8df2631c6cfb23040dd0515fd6aa254aa8f855f4
Dmitriy <koogich@mail.ru>
2026-02-19 12:45:21 +0300

    feat: coin model && cargo dependencies

---
+++ src/models/coin.rs
@@ -0,0 +1,24 @@
+use diesel::prelude::*;
+use uuid::Uuid;
+use chrono::{DateTime, Utc};
+use crate::schema::coins;
+
+#[derive(Queryable, Identifiable, Debug)]
+#[diesel(table_name = coins)]
+#[diesel(primary_key(id))]
+pub struct Coin {
+    pub id: Uuid,
+    pub symbol: String,
+    pub name: String,
+    pub decimals: i32,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = coins)]
+pub struct NewCoin<'a> {
+    pub symbol: &'a str,
+    pub name: &'a str,
+    pub decimals: i32,
+}
